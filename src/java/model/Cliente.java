e71c2d3c7f80348f40c80031dd3be1a47167c968
Thiago Reis <Thiago Reis@DESKTOP-ATFOIQ1>
2016-10-16 01:25:17 -0200

    listar cliente incluir cliente index - dashboard

--- src/java/model/Cliente.java
+++ src/java/model/Cliente.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -36,6 +37,7 @@ public class Cliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue
     @Basic(optional = false)
     @NotNull
     @Column(name = "CODIGO_CLIENTE")
