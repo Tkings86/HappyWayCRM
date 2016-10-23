08378ce059501e932bca4f96bf7d3e9b97cf58ad
Thiago Reis <Thiago Reis@DESKTOP-ATFOIQ1>
2016-10-23 12:32:59 -0200

    paginas JSF

--- src/java/model/EndCliente.java
+++ src/java/model/EndCliente.java
@@ -6,15 +6,18 @@
 package model;
 
 import java.io.Serializable;
+import javax.annotation.Generated;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.GroupSequence;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
